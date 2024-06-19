e4b515fe0d7eb9d18a1005053e9112b676b9faea
Pavel Toshinski <pavelce2000@hotmail.com>
2024-06-19 18:56:29 +0200

    Added new file and exercise problem

---
+++ script.js
@@ -0,0 +1,19 @@
+// Домашна
+// 1. Сите студенти од Скопје чие име завршува на а и имаат просек над 7, подредени по име (растечки).
+// 2. Сите студенти кои имаат просек над 9 и не се од Скопје, подредени по просек опаѓачки.
+// 3. Првите 3 студенти кои имаат имиња од 5 карактери, подредени по просек.
+// 4. Градови подредени по групна висина на просек.
+// 5. Вкупен просек на студенти чие име завршува на а наспроти сите останати.
+
+// const studenti = [
+//   { ime: "Bojan", prosek: 7.5, grad: "Skopje" }, 
+//   { ime: "Pero", prosek: 8.3, grad: "Bitola" },
+//   { ime: "Janko", prosek: 6.9, grad: "Bitola" },
+//   { ime: "Vesna", prosek: 9.2, grad: "Skopje" },
+//   { ime: "Elena", prosek: 9.9, grad: "Kumanovo" },
+//   { ime: "Vancho", prosek: 10, grad: "Tetovo" },
+//   { ime: "Elena", prosek: 9.9, grad: "Ohrid" },
+//   { ime: "Ivana", prosek: 6.9, grad: "Kumanovo" },
+//   { ime: "Natasha", prosek: 8.1, grad: "Skopje" },
+//   { ime: "Stanko", prosek: 7.2, grad: "Strumica" },
+// ];
