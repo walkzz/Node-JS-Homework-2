dd664c62e4b063a4075e25384d3998eb999b97e6
Pavel Toshinski <pavelce2000@hotmail.com>
2024-06-19 19:15:32 +0200

    Task 2 done

--- script.js
+++ script.js
@@ -38,10 +38,15 @@ const findCityAndAverage = studenti
 .filter(student => student.grad === "Skopje" && findLastLetter(student.ime) === 'a' && student.prosek > 7)
 .sort((a, b) => {
     if (a.ime < b.ime) return -1;
-    if (a.ime > b.ime) return 1;
+    if (a.ime > b.ime) return 1; // experimental
     return 0;
 });
 console.log(findCityAndAverage);
 console.log("--------------------------------------------");
 // task 2
 console.log("task 2:");
+const aboveNineAndCity = studenti
+    .filter(student => student.prosek > 9 && student.grad !== "Skopje")
+    .sort((a, b) => b.prosek - a.prosek);
+
+console.log(aboveNineAndCity);
